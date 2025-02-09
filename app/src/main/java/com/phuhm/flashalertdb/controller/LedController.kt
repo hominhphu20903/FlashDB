d95cfc392436aedcf097ca82fb27c2b5638ac3b9
PhuHM <hominhphu20903@gmail.com>
2025-02-09 00:34:37 -0800

    Cm

--- app/src/main/java/com/phuhm/flashalertdb/controller/LedController.kt
+++ app/src/main/java/com/phuhm/flashalertdb/controller/LedController.kt
@@ -2,6 +2,7 @@ package com.phuhm.flashalertdb.controller
 
 import android.animation.ValueAnimator
 import android.graphics.Color
+import android.util.Log
 import android.view.View
 
 class LedController {
@@ -9,23 +10,27 @@ class LedController {
     private var animator: ValueAnimator? = null
 
     fun startSmoothFlashing(view: View) {
-        val colors = arrayOf(Color.RED, Color.GREEN, Color.BLUE)
-        animator = ValueAnimator.ofFloat(0f, 3f).apply {
-            duration = 500
-            repeatCount = ValueAnimator.INFINITE
-            repeatMode = ValueAnimator.RESTART
+        try {
+            val colors = arrayOf(Color.RED, Color.GREEN, Color.BLUE)
+            animator = ValueAnimator.ofFloat(0f, 3f).apply {
+                duration = 500
+                repeatCount = ValueAnimator.INFINITE
+                repeatMode = ValueAnimator.RESTART
 
-            addUpdateListener { animation ->
-                val fraction = animation.animatedValue as Float
-                val index = fraction.toInt() % colors.size
-                val nextIndex = (index + 1) % colors.size
-                val blendRatio = fraction - index
+                addUpdateListener { animation ->
+                    val fraction = animation.animatedValue as Float
+                    val index = fraction.toInt() % colors.size
+                    val nextIndex = (index + 1) % colors.size
+                    val blendRatio = fraction - index
 
-                val color = blendColors(colors[index], colors[nextIndex], blendRatio)
-                view.setBackgroundColor(color)
+                    val color = blendColors(colors[index], colors[nextIndex], blendRatio)
+                    view.setBackgroundColor(color)
+                }
             }
+            animator?.start()
+        } catch (e: Exception) {
+            Log.d("LedController", "startSmoothFlashing: ")
         }
-        animator?.start()
     }
 
     fun stopFlashing() {
--- app/src/main/java/com/phuhm/flashalertdb/screen/calls/FlashCallsActivity.kt
+++ app/src/main/java/com/phuhm/flashalertdb/screen/calls/FlashCallsActivity.kt
@@ -1,16 +1,45 @@
 package com.phuhm.flashalertdb.screen.calls
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import com.phuhm.flashalertdb.base.BaseActivity
 import com.phuhm.flashalertdb.databinding.ActivityFlashCallsBinding
 
 class FlashCallsActivity : BaseActivity<ActivityFlashCallsBinding>() {
+    private var isShowImageWarningTop: Boolean = false
+    private val handler = Handler(Looper.getMainLooper())
+    private val runnable = object : Runnable {
+        override fun run() {
+            try {
+                isShowImageWarningTop = !isShowImageWarningTop
+                if(isShowImageWarningTop) {
+                    binding.imvWarningTop.visibility = View.VISIBLE
+                    binding.imvWarningBottom.visibility = View.INVISIBLE
+                } else {
+                    binding.imvWarningTop.visibility = View.INVISIBLE
+                    binding.imvWarningBottom.visibility = View.VISIBLE
+                }
+                handler.postDelayed(this, 500)
+            } catch (e: Exception) {
+                Log.d("FlashCallsActivity", "run: ")
+            }
+        }
+    }
     override fun inflateBinding(inflater: LayoutInflater): ActivityFlashCallsBinding {
         return ActivityFlashCallsBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        handler.post(runnable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
     }
 }
